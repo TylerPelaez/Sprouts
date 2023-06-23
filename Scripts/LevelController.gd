8dace6575865fd1f24ad527d2939a8bc912056c8
Tyler Pelaez <mail@tylerpelaez.com>
2023-06-23 14:56:20 -0400

    Rename Test Scene To Tyler Test, Create Level End Prefab

--- Scripts/LevelController.gd
+++ Scripts/LevelController.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var player_has_jump: bool = true
 @export var player_has_double_jump: bool = false
 @export var player_has_companion: bool = true
+@export_file var next_level
 
 @onready var player_prefab: PackedScene = preload("res://Prefabs/Player.tscn")
 @onready var companion_prefab: PackedScene = preload("res://Prefabs/Companion.tscn")
@@ -27,6 +28,10 @@ func _ready():
 		companion = companion_prefab.instantiate()
 		add_child(companion)
 		companion.initialize(player)
+		
+	for child in get_children():
+		if child is LevelEnd:
+			child.level_ended.connect(_on_level_end)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,3 +39,9 @@ func _process(delta):
 
 func _on_player_death():
 	get_tree().reload_current_scene()
+
+func _on_level_end():
+	if next_level != null:
+		get_tree().change_scene_to_file(next_level)
+	else:
+		get_tree().reload_current_scene()
