7ff778a8cbc0622f15aeeddf704cd822236cf1ba
Tyler Pelaez <mail@tylerpelaez.com>
2023-06-22 20:29:52 -0400

    Add a level kill box prefab and player spawner

---
+++ Scripts/LevelController.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var player_has_jump: bool = true
+@export var player_has_double_jump: bool = false
+
+@onready var player_prefab: PackedScene = preload("res://Prefabs/Player.tscn")
+
+@onready var camera: Camera2D = $Camera2D
+@onready var player_spawn_pos: Marker2D = $PlayerSpawnPosition
+
+var player: Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = player_prefab.instantiate()
+	add_child(player)
+	player.has_jump = player_has_jump
+	player.has_double_jump = player_has_double_jump
+	player.set_camera_follow(camera)
+	player.global_position = player_spawn_pos.global_position
+	player.died.connect(_on_player_death)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_death():
+	get_tree().reload_current_scene()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,30 +1,49 @@
 extends CharacterBody2D
+class_name Player
+
+signal died
+
+@export var SPEED: float = 300.0
+@export var JUMP_VELOCITY: float = -400.0
+
+@export var has_jump: bool = true
+@export var has_double_jump: bool = false
 
 @onready var coyote_timer := $CoyoteTimer
+@onready var remote_transform := $RemoteTransform2D
 
-const SPEED: float = 300.0
-const JUMP_VELOCITY: float = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var jumped := false
+var double_jumped := false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		coyote_timer.start()
 		jumped = false
+		double_jumped = false
 
 	# Handle Jump.
-	if !jumped and Input.is_action_just_pressed("Jump") and (is_on_floor() or !coyote_timer.is_stopped()):
+	
+	var do_jump: bool = false
+	if has_jump and Input.is_action_just_pressed("Jump"):
+		if is_on_floor() or !coyote_timer.is_stopped():
+			if !jumped:
+				jumped = true
+				do_jump = true
+			elif !double_jumped and has_double_jump:
+				# Handle someone hitting double jump reaaaly soon after first jump
+				double_jumped = true
+				do_jump = true
+		elif !double_jumped and has_double_jump:
+			double_jumped = true
+			do_jump = true
+	
+	if do_jump:
 		velocity.y = JUMP_VELOCITY
-		jumped = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -32,3 +51,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func set_camera_follow(camera: Node2D):
+	remote_transform.remote_path = remote_transform.get_path_to(camera)
+
+func die():
+	died.emit()
+
+func _on_kill_area_check_area_entered(area):
+	die()
